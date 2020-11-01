720bbfa3c3bdf870f2597f92c9fd8696a958838a
mgrybel <monika.grybel@hotmail.com>
2020-11-01 22:37:31 +0000

    Added password hashing tests

--- app/config.py
+++ app/config.py
@@ -16,7 +16,7 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
-class TestingConfig:
+class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
